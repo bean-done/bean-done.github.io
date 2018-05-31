653c28191f73e7015aeba011d6b28623a32c3ef7
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-05-31 15:35:13 -0400

    Add about page with header link

---
+++ src/pages/about.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>
+          Integer elit leo, ultricies quis nisi vel, efficitur feugiat turpis.
+          Aenean ac interdum diam. Ut aliquam velit non eleifend pharetra.
+          Curabitur ut ultrices nisl. Aenean eget lectus tortor. Etiam porttitor
+          eros quam, imperdiet vestibulum neque pellentesque eget. Quisque
+          rutrum nibh erat, eget laoreet nisi dignissim in. Cras bibendum auctor
+          egestas. Morbi a gravida odio. Nam nec nisl facilisis, hendrerit ipsum
+          vel, vestibulum urna. Donec eget nisi orci. Cras ipsum velit, laoreet
+          at euismod ac, cursus eget libero.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default About
