4dc1bab75a273f111ece4b6fa4dd13953e18d7ee
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-06-04 13:04:28 -0400

    Updates

--- constants.js
+++
@@ -1,8 +0,0 @@
-const HOME_PATH = ''
-// HEADER_PATH requires the trailing slash when reading locations using React router
-const HEADER_PATH = HOME_PATH + '/'
-
-module.exports = {
-  HOME_PATH,
-  HEADER_PATH,
-}
